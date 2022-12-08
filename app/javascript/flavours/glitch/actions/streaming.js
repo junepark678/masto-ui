cac148314759b03b9c2e78a563c6cec587188d19
FloatingGhost <hannah@coffee-and-dreams.uk>
2022-12-08 14:50:38 +0000

    don't update all timelines

--- app/javascript/flavours/glitch/actions/streaming.js
+++ app/javascript/flavours/glitch/actions/streaming.js
@@ -85,11 +85,21 @@ export const connectTimelineStream = (timelineId, channelName, params = {}, opti
         }
       },
 
-      onReceive(data) {
-        switch (data.event) {
+      shouldUpdate (timelineId, streamName) {
+        const stream = streamName[0];
+        if (timelineId === 'home' && streamName.startsWith('user')) {
+          return true;
+        }
+
+        return timelineId === stream;
+      },
+
+      onReceive (data) {
+        switch(data.event) {
         case 'update':
-          // @ts-expect-error
-          dispatch(updateTimeline(timelineId, JSON.parse(data.payload), options.accept));
+          if (this.shouldUpdate(timelineId, data.stream)) {
+            dispatch(updateTimeline(timelineId, JSON.parse(data.payload), options.accept));
+          }
           break;
         case 'status.update':
           // @ts-expect-error
